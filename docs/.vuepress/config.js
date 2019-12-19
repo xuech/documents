27889c703ae028f9caa49e7e1195976ae9a99cff
xuech <xuech_love@126.com>
2019-12-19 15:28:25 +0800

    配置deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -48,6 +48,16 @@ module.exports = {
                     '/javascript/js01'
                 ]
             }
-        ]
+        ],
+        // 你的 Git 项目地址，添加后会在导航栏的最后追加
+        repo: 'xuech/documents',
+        // 启用编辑
+        editLinks: true,
+        // 编辑按钮的 Text
+        editLinkText: '编辑文档！',
+        // 编辑文档的所在目录
+        docsDir: 'docs',
+        // 假如文档放在一个特定的分支下：
+        // docsBranch: 'master',
     }
 }
